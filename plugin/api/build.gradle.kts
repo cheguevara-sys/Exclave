849867198bc66fcc1223729f026d48a62608e10a
Che Guevara <cheguevara@student.mksu.ac.ke>
2026-07-30 00:51:08 +0300

    Update build.gradle.kts

--- plugin/api/build.gradle.kts
+++ plugin/api/build.gradle.kts
@@ -2,8 +2,25 @@ plugins {
     id("com.android.library")
 }
 
-setupCommon()
-
 android {
     namespace = "io.nekohasekai.sagernet.plugin"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    // Add any needed dependencies here
 }
