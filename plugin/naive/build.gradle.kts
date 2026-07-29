4087fa8b0628fff1faf79d269de28c9ff1b1cacb
Che Guevara <cheguevara@student.mksu.ac.ke>
2026-07-30 01:16:27 +0300

    Update build.gradle.kts

--- plugin/naive/build.gradle.kts
+++ plugin/naive/build.gradle.kts
@@ -1,12 +1,28 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")   // ← Add this
 }
 
 android {
+    namespace = "io.nekohasekai.sagernet.plugin.naive"
+    compileSdk = 34
+
     defaultConfig {
         applicationId = "io.nekohasekai.sagernet.plugin.naive"
+        minSdk = 21
+        targetSdk = 34
     }
-    namespace = "io.nekohasekai.sagernet.plugin.naive"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
-setupPlugin("naive")
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+}
